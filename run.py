2f60cefb4bcc8f8c937404c4cef5494adbadf69a
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2022-10-17 00:30:10 +0000

    Add further validation for player ship selection

--- run.py
+++ run.py
@@ -13,6 +13,9 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 SHEET = GSPREAD_CLIENT.open('Battleships')
 
+
+SHIP_SIZES = [1, 1, 2, 2, 3, 4, 5]
+
 def set_grid_size():
     while True:
         print('Please select the size of the grid that you would like to play on.')
@@ -36,30 +39,30 @@ def validate_grid(grid):
     
     return True
 
-def select_player_ships(grid):
+def select_player_ships(grid, grid_dict):
     player_ships = {}
 
     while True:
         print('You will now be asked to place each one of your ships.')
-        print('For the first two ships, you must enter two numbers, separated by a space, to serve as coordinates, e.g. 2 10')
+        print('For the first two ships, you must enter two numbers, separated by a space, to serve as x and y coordinates, e.g. 2 10')
         print('For the remaining five ships, you must also enter either H or V, for horizontal or vertical, e.g. 2 10 V')
         print(f'(Keep in mind that you have selected a {grid}x{grid} grid, which means that numbers greater than {grid} will not be accepted)\n')
 
-        player_ships['submarine_1'] = input('Please position your first Submarine (occupies 1 square): ').split(' ')
-        player_ships['submarine_2'] = input('Please position your second Submarine (occupies 1 square): ').split(' ')
-        player_ships['destroyer_1'] = input('Please position your first Destroyer (occupies 2 squares): ').split(' ')
-        player_ships['destroyer_2'] = input('Please position your second Destroyer (occupies 2 squares): ').split(' ')
-        player_ships['cruiser'] = input('Please position your Cruiser (occupies 3 squares): ').split(' ')
-        player_ships['battleship'] = input('Please position your Battleship (occupies 4 squares): ').split(' ')
-        player_ships['aircraft_carrier'] = input('Please position your Aircraft Carrier (occupies 5 squares): ').split(' ')
+        player_ships['first submarine'] = input('Please position your first Submarine (occupies 1 square): ').split(' ')
+        player_ships['second submarine'] = input('Please position your second Submarine (occupies 1 square): ').split(' ')
+        player_ships['first destroyer'] = input('Please position your first Destroyer (occupies 2 squares): ').upper().split(' ')
+        player_ships['second destroyer'] = input('Please position your second Destroyer (occupies 2 squares): ').upper().split(' ')
+        player_ships['cruiser'] = input('Please position your Cruiser (occupies 3 squares): ').upper().split(' ')
+        player_ships['battleship'] = input('Please position your Battleship (occupies 4 squares): ').upper().split(' ')
+        player_ships['aircraft_carrier'] = input('Please position your Aircraft Carrier (occupies 5 squares): ').upper().split(' ')
 
-        if validate_ships(grid, player_ships):
+        if validate_ships(grid, grid_dict, player_ships):
             print(f'\nShips selected.\n')
             break
 
     return player_ships
     
-def validate_ships(grid, player_ships):
+def validate_ships(grid, grid_dict, player_ships):
     coordinates = list(player_ships.values())
 
     try:
@@ -72,10 +75,29 @@ def validate_ships(grid, player_ships):
         
         for coordinate in coordinates:
             for x in coordinate:    
+                if x.isalpha():
+                    continue
                 if int(x) > grid:
                     raise ValueError(f'\nA coordinate you entered lies outside of the {grid}x{grid} grid.')
-        
-    except ValueError as e:
+
+        for x, player_ship in enumerate(player_ships, start=0):
+            if x >= 2:
+                if player_ships[player_ship][2] != 'H' and player_ships[player_ship][2] != 'V':
+                    raise ValueError(f'\nYou did not provide a valid horizontal or vertical value (H or V) for the {player_ship} that you placed.')
+                elif len(player_ships[player_ship]) <= 2:
+                    raise IndexError(f'\nYou did not provide a horizontal or vertical value (H or V) for {player_ship} that you placed.')
+
+        for x, player_ship in enumerate(player_ships, start=0):
+            if x >= 2:
+                if int(player_ships[player_ship][0]) + SHIP_SIZES[x] - 1 > grid and player_ships[player_ship][2] == 'H' or int(player_ships[player_ship][1]) + SHIP_SIZES[x] - 1 > grid and player_ships[player_ship][2] == 'V':
+                    raise ValueError(f'\nThe {player_ship} that you placed is partially outside of the {grid}x{grid} grid.')
+
+        for grid_coordinates in grid_dict:
+            coordinates = grid_coordinates.split(' ')
+            for player_ship in player_ships:
+                
+            
+    except (ValueError, IndexError) as e:
         print(f'{e} Please place your ships again.\n')
         return False
 
@@ -86,8 +108,19 @@ def place_player_ships(grid, player_ships):
 
 def main():
     grid = set_grid_size()
-    player_ships = select_player_ships(grid)
+    grid_dict = {f'{x} {y}': None for x in range(1, grid + 1) for y in range(1, grid + 1)}
+    player_ships = select_player_ships(grid, grid_dict)  
     place_player_ships(grid, player_ships)
 
 print(f'Welcome to Battleships!\n')
+
+print(grid_dict)
+
+# for grid_coordinate in grid_dict:
+#     print(grid_coordinate) PRINTS 11
+#     for x in grid_coordinate:
+#         print(x) PRINTS 1
+#         break
+#     break
+
 main()
