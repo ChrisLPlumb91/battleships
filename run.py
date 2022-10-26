3bcba5541fb0493d1fd1852b8df51119ff235444
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2022-10-26 23:40:47 +0000

    Added exit function at beginning of game, changed validation of initial player input so that each input is validated as it is entered, and added docstrings to all functions

--- run.py
+++ run.py
@@ -21,15 +21,32 @@ grid_dict_cpu = {}
 
 
 def set_grid_size():
-    while True:
-        print('Please select the size of the grid that you would like to play on.')
-        print(f'(Enter 10 for 10x10, 12 for 12x12, or 14 for 14x14)\n')
+    """
+    Prompts the user to enter one of three 2-digit integers, and validates this input.
+    This input is cast to an int, and the try / except catches any input that cannot
+    be cast in this way.
 
-        try:
-            grid = int(input('Enter your selection here: '))
-        except ValueError:
-            print(f'Please enter either 10, 12, or 14.\n')
-            continue
+    If the user enters 0, the exit_game() function is called.
+
+    Because any number can be entered, a function that further validates the input
+    to ensure that it is one of the three options is called. If it returns false,
+    the user is prompted again. If it returns True, the number representing the
+    grid is returned by this function.
+    """    
+    while True:
+        grid = 0
+        while grid == 0:
+            print(f'\nPlease select the size of the grid that you would like to play on.')
+            print(f'(Enter 10 for 10x10, 12 for 12x12, 14 for 14x14, or 0 to exit the game)\n')
+
+            try:
+                grid = int(input(f'Enter your selection here:\n'))
+            except ValueError:
+                print(f'Please enter either 10, 12, or 14 (or 0 if you wish to quit).\n')
+                continue
+
+            if grid == 0:
+                exit_game()
 
         if validate_grid(grid):
             print(f'{grid}x{grid} grid selected.\n')
@@ -38,7 +55,33 @@ def set_grid_size():
     return grid
 
 
+def exit_game():
+    """
+    If the user enters 0 during the grid size function, they are taken to this
+    function. If they enter N, the game will return to the grid selection function.
+
+    The while loop here ensures that the game can only proceed (exit or continue),
+    if the user enters the correct value.
+    """
+    choice = input(f'Do you really want to quit the game? (enter Y or N)\n').upper()
+
+    while True:
+        if choice == 'N':
+            return True
+        elif choice == 'Y':
+            print(f'\nThanks for playing!\n')
+            exit()
+        else:
+            print('Please enter either Y or N.')
+            continue
+
+
 def validate_grid(grid):
+    """
+    This is the function that further validates the user's input for their choice of grid.
+    If their choice is not one of the three, a ValueError is raised and caught. They are then
+    carried back to the grid selection function.
+    """
     try:
         if grid != 10 and grid != 12 and grid != 14:
             raise ValueError(f'{grid} is not a valid option.')
@@ -50,6 +93,17 @@ def validate_grid(grid):
 
 
 def select_player_ships(grid, grid_dict_player):
+    """
+    This function prompts the user to enter coordinates for each of their ships.
+    Each request for input is wrapped in a while loop and is followed by a call
+    to a validating function, and the input itself becomes the value of a key
+    in a dictionary. Each key bears the name of a ship.
+
+    After each ship is successfully validated individually, a function is called
+    that ensures that none of the ships' coordinates are identical or overlapping.
+
+    This function ultimately returns the dictionary of ships and their coordinates.
+    """
     player_ships = {}
 
     while True:
@@ -58,13 +112,54 @@ def select_player_ships(grid, grid_dict_player):
         print('For the remaining five ships, you must also enter either H or V, for horizontal or vertical, e.g. 2 10 V')
         print(f'(Keep in mind that you have selected a {grid}x{grid} grid, which means that numbers greater than {grid} will not be accepted)\n')
 
-        player_ships['first Submarine'] = input(f'Please position your first Submarine (occupies 1 square):\n').split(' ')
-        player_ships['second Submarine'] = input(f'Please position your second Submarine (occupies 1 square):\n').split(' ')
-        player_ships['first Destroyer'] = input(f'Please position your first Destroyer (occupies 2 squares):\n').upper().split(' ')
-        player_ships['second Destroyer'] = input(f'Please position your second Destroyer (occupies 2 squares):\n').upper().split(' ')
-        player_ships['Cruiser'] = input(f'Please position your Cruiser (occupies 3 squares):\n').upper().split(' ')
-        player_ships['Battleship'] = input(f'Please position your Battleship (occupies 4 squares):\n').upper().split(' ')
-        player_ships['Aircraft Carrier'] = input(f'Please position your Aircraft Carrier (occupies 5 squares):\n').upper().split(' ')
+        while True:
+            x = 2
+            player_ships['first Submarine'] = input(f'Please position your first Submarine (occupies 1 square):\n').split(' ')
+            
+            if validate_ship(grid, x, player_ships['first Submarine'], 'first Submarine'):
+                break
+        
+        while True:
+            x = 2
+            player_ships['second Submarine'] = input(f'Please position your second Submarine (occupies 1 square):\n').split(' ')
+
+            if validate_ship(grid, x, player_ships['second Submarine'], 'second Submarine'):
+                break
+
+        while True:
+            x = 3
+            player_ships['first Destroyer'] = input(f'Please position your first Destroyer (occupies 2 squares):\n').upper().split(' ')
+
+            if validate_ship(grid, x, player_ships['first Destroyer'], 'first Destroyer'):
+                break
+
+        while True:
+            x = 3
+            player_ships['second Destroyer'] = input(f'Please position your second Destroyer (occupies 2 squares):\n').upper().split(' ')
+
+            if validate_ship(grid, x, player_ships['second Destroyer'], 'second Destroyer'):
+                break
+
+        while True:
+            x = 3
+            player_ships['Cruiser'] = input(f'Please position your Cruiser (occupies 3 squares):\n').upper().split(' ')
+
+            if validate_ship(grid, x, player_ships['Cruiser'], 'Cruiser'):
+                break
+        
+        while True:
+            x = 3
+            player_ships['Battleship'] = input(f'Please position your Battleship (occupies 4 squares):\n').upper().split(' ')
+
+            if validate_ship(grid, x, player_ships['Battleship'], 'Battleship'):
+                break
+        
+        while True:
+            x = 3
+            player_ships['Aircraft Carrier'] = input(f'Please position your Aircraft Carrier (occupies 5 squares):\n').upper().split(' ')
+
+            if validate_ship(grid, x, player_ships['Aircraft Carrier'], 'Aircraft Carrier'):
+                break
 
         if validate_player_ships(grid, grid_dict_player, player_ships):
             print(f'\nPositioning your ships. Please wait...')
@@ -73,65 +168,74 @@ def select_player_ships(grid, grid_dict_player):
     return player_ships
     
 
-def validate_player_ships(grid, grid_dict_player, player_ships):
-    coordinates = list(player_ships.values())
+def validate_ship(grid, sub_or_ship, coordinate_list, ship_name):
+    """
+    This is the function that is called for each pair of coordinates that the user enters.
+    It checks to makes sure that the user has not: entered a single number, entered more
+    numbers than required, entered fewer numbers than required, neglected to provide a H
+    or V value for the orientation of the ships that take up 2 squares or more,
+    or entered a letter where a number was expected.
 
+    If an exception is raised, the user will be taken back to the ship select function
+    and asked to enter proper coordinates for the ship that caused the error.
+    """
     try:
-        for coordinate in coordinates:
-            coordinate_dict = dict(enumerate(coordinate))
-            if not coordinate_dict.get(1):
-                raise ValueError(f'\nEither you entered only a single character, or you did not put a space between the characters ({coordinate}).')
-            else:
-                continue
-    
-        for x, coordinate in enumerate(coordinates, start=0):
-            if x <= 1 and len(coordinate) > 2:
-                raise ValueError(f'\nYou entered {len(coordinate)} numbers / characters ({coordinate}) where only 2 were required.')
-            elif x > 1 and len(coordinate) > 3:
-                raise ValueError(f'\nYou entered {len(coordinate)} numbers / characters ({coordinate}) where only 3 were required.')
-            else:
-                continue
+        coordinate_dict = dict(enumerate(coordinate_list))
+        if not coordinate_dict.get(1):
+            raise ValueError(f'\nEither you entered only a single character, or you did not put a space between the characters.')
 
-        for x, player_ship in enumerate(player_ships, start=0):
-            if x >= 2:
-                if player_ships[player_ship][2] != 'H' and player_ships[player_ship][2] != 'V':
-                    raise ValueError(f'\nYou did not provide a valid horizontal or vertical value (H or V) for the {player_ship} that you placed.')
-                elif len(player_ships[player_ship]) <= 2:
-                    raise IndexError(f'\nYou did not provide a horizontal or vertical value (H or V) for {player_ship} that you placed.')
-            else: 
-                continue
-        
-        for x, coordinate in enumerate(coordinates, start=0):
-            if x > 1 and len(coordinate) < 3:
-                raise ValueError(f'\nYou entered {len(coordinate)} numbers / characters ({coordinate}) where 3 were required.')
-            else:
-                continue
+        if sub_or_ship == 2 and len(coordinate_list) > 2:
+            raise ValueError(f'\nYou entered {len(coordinate_list)} numbers / characters where only 2 were required.')
+        elif sub_or_ship == 3 and len(coordinate_list) > 3:
+            raise ValueError(f'\nYou entered {len(coordinate_list)} numbers / characters where only 3 were required.')
 
-        for x, coordinate in enumerate(coordinates, start=0):
-            if x <= 1 and not coordinate[0].isnumeric() or x <= 1 and not coordinate[1].isnumeric():
-                raise ValueError(f'\nYou entered a letter where a number was expected ({coordinate}).')
-            else:
+        if sub_or_ship == 3 and len(coordinate_list) < 3:
+            raise ValueError(f'\nYou entered {len(coordinate_list)} numbers / characters where 3 were required.')
+
+        if sub_or_ship == 3 and coordinate_list[2] != 'H' and coordinate_list[2] != 'V':
+            raise ValueError(f'\nYou did not provide a valid horizontal or vertical value (H or V) for the {ship_name} that you placed.')
+
+        if not coordinate_list[0].isnumeric() or not coordinate_list[1].isnumeric():
+            raise ValueError(f'\nYou entered a letter where a number was expected.')
+
+        if coordinate_list[0] <= 0 or coordinate_list[1] <= 0:
+            raise ValueError(f"\nYou entered a coordinate that doesn't exist on the grid.")
+
+        for y in coordinate_list:    
+            if y.isalpha():
                 continue
+            if int(y) > grid:
+                raise ValueError(f'\nThese coordinates would place this ship outside of the {grid}x{grid} grid.')
+
+    except ValueError as e:
+        print(f'{e} Try again.\n')
+        return False
+
+    return True
+
+
+def validate_player_ships(grid, grid_dict_player, player_ships):
+    """
+    This is the function that checks to make sure that the coordinates of each of the ships
+    do not clash with one another. It also checks to make sure that a given ship does not 
+    extend beyond the boundaries of the grid, e.g placing an Aircraft Carrier horizontally 
+    at 10, 1 on a 10x10 grid.
+
+    Another important thing that it does is "occupy" keys in a dictionary. Each key is
+    a coordinate (from 1, 1 to 10, 10, in the case of a 10x10 grid), and this function
+    assigns values to the coordinates that are occupied by ships. It figures this out
+    by referring to each ship's length, as well as its starting coordinate (i.e. the coordinate
+    provided by the user).
+    """
+    coordinates = list(player_ships.values())
         
-        for coordinate in coordinates:
-            if coordinate[0].isalpha() or coordinate[1].isalpha():
-                raise ValueError(f'\nThe coordinates you provided contained an alphabetic character where a number was expected.')
-            else:
-                continue
-        
+    try:
         for x, coordinate in enumerate(coordinates, start=0):
             for y, coordinate_comp in enumerate(coordinates, start=0):
                 if x == y:
                     continue
                 elif coordinate == coordinate_comp:
-                    raise ValueError(f'\nYou chose identical coordinates for two different ships.')
-        
-        for coordinate in coordinates:
-            for x in coordinate:    
-                if x.isalpha():
-                    continue
-                if int(x) > grid:
-                    raise ValueError(f'\nA coordinate you entered lies outside of the {grid}x{grid} grid.')
+                    raise ValueError(f'\nYou chose identical coordinates for two different ships.') 
 
         for x, player_ship in enumerate(player_ships, start=0):
             if x >= 2:
@@ -179,6 +283,16 @@ def validate_player_ships(grid, grid_dict_player, player_ships):
 
 
 def place_player_ships(grid, player_ships, grid_dict_player):
+    """
+    This function is responsible for actually inserting the user's ships into the spreadsheet.
+    Depending on the ship, a different initial is inserted into the correct cells of the
+    spreadsheet - the spreadsheet itself representing the grid.
+
+    Throughout its execution, this function creates lists that are either 10, 12, or 14 in length
+    (depending on the grid size), and then appends them to the 'player' spreadsheet. Where in each
+    row a ship should be, and which ship that ship should be, is determined from the dictionary
+    of coordinates and ships created previously.
+    """
     player_grid = SHEET.worksheet('player')
     row_values = list(grid_dict_player.values())
     row_list = []
@@ -247,6 +361,15 @@ def place_player_ships(grid, player_ships, grid_dict_player):
 
 
 def select_cpu_ships(grid, player_ships):
+    """
+    This function assigns coordinates to the CPU's ships. It makes use of the random library
+    to generate those coordinates. For the ships that take up more than 1 square, either
+    H or V is selected randomly to provide their orientation on the grid.
+
+    Then, a function is called on the resulting dictionary to validate its contents.
+    If the validation function returns True, this function returns a dictionary of
+    the CPU's ships and their starting coordinates.
+    """
     cpu_ships = {}
     cpu_ship_names = list(player_ships.keys())
     global grid_dict_cpu
@@ -282,6 +405,18 @@ def select_cpu_ships(grid, player_ships):
     
 
 def validate_cpu_ships(grid, cpu_ships):
+    """
+    This is the function that validates the coordinates randomly generated for the CPU's ships.
+    It does not use exception handling, as the CPU ship selection process is more controlled than
+    the user's, i.e. the CPU can only "input" from a very small pool of possible values.
+
+    Like with the function that validates all of the user's ships together, this function
+    generates a dictionary of coordinates, and "occupies" the relevant coordinates with values.
+
+    Over the course of this function, several flags are set if conflicts are detected, and the validation
+    can only be successful if all of these flags are False by the end. If they are True,
+    a different set of coordinates are randomly generated for the CPU.
+    """
         
     global grid_dict_cpu
     cpu_ships_coordinates = list(cpu_ships.values())
@@ -346,7 +481,28 @@ def validate_cpu_ships(grid, cpu_ships):
             return False
     return True
 
+
 def start_game(grid, player_ships, cpu_ships, grid_dict_player):
+    """
+    This function contains code for the playable part of the program. It prompts the user
+    to input numbers as coordinates, just like when they had to position their ships.
+    These coordinates are checked against the CPU's dictionary of coordinates,
+    and if occupied coordinates are found, the number of hits against the CPU
+    ticks up, and the initial of ship that was hit is inserted in the 'cpu' spreadsheet,
+    at the coordinates where that ship was found.
+
+    The CPU does the same, but its guess is randomly generated, and if it lands a hit,
+    the respective initial on the 'player' spreadsheet becomes an 'X'. Also, a
+    validation function is not called to validate the CPU guess.
+
+    When a ship is sunk, the user is notified (whether it belongs to them or to the CPU).
+    There is a dictionary containing lists of Os that serve as the hit points for each ship.
+    When a ship is hit, an O is popped off the relevant list, and when the list's length
+    becomes zero, the ship is sunk.
+
+    As there are 18 possible targets for both the user and the CPU, once one of the hit counters
+    ticks up to 18, a winner is declared, and both spreadsheets are cleared of their contents.
+    """
     print(f'\nMan the poop deck! War has returned to the high seas!\n')
 
     player_grid = SHEET.worksheet('player')
@@ -365,9 +521,6 @@ def start_game(grid, player_ships, cpu_ships, grid_dict_player):
         hit_dict_cpu[cpu_ship] = []
         for y in range(SHIP_SIZES[x]):
             hit_dict_cpu[cpu_ship].append('O')
-
-    print(hit_dict_player)
-    print(hit_dict_cpu)
 
     player_hits = 0
     cpu_hits = 0
@@ -451,7 +604,7 @@ def start_game(grid, player_ships, cpu_ships, grid_dict_player):
         if player_hits == 0:
             print(f'FLAWLESS VICTORY!!\n')
         elif player_hits > 0:
-            print(f'...but at what cost? The CPU landed {player_hits} on your fleet...\n')
+            print(f'...however, The CPU landed {player_hits} hits on your fleet...\n')
     elif player_hits >=18:
         print('The CPU has scuppered your entire fleet! You lose!')
 
@@ -460,6 +613,13 @@ def start_game(grid, player_ships, cpu_ships, grid_dict_player):
             
 
 def validate_guess(player_guess, grid):
+    """
+    This function validates each guess the user makes during the playable phase of the program.
+    It is similar to the function that validates each coordinate provided by the user at the game's outset,
+    but it is subtly different, and so had to be its own function. Specifically, some of the messages in the former 
+    one would not make sense in the context of the playable phase. Moreover, letters denoting H and V
+    are not requested during said phase.
+    """
     player_guess_dict = dict(enumerate(player_guess))
     
     try:
@@ -484,6 +644,13 @@ def validate_guess(player_guess, grid):
 
 
 def main():
+    """
+    This is the main function for the game. Excluding the validation functions, all of the
+    major functions of the program are called from inside this one. Return values from functions
+    are passed to other functions. Once all of these functions have successfully executed, 
+    the user is asked if they would like to play again. If they do, the game restarts 
+    from the beginning, and if they do not, execution stops.
+    """
     continue_game = 'Y'
     
     while continue_game == 'Y':
@@ -515,5 +682,5 @@ def main():
     exit()
 
 
-print(f'\nWelcome to Battleships!\n')
+print(f'\nWelcome to Battleships!')
 main()
